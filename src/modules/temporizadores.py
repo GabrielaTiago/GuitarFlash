d85ea4da5766c6cb4bb2289152be672ebbbb05d9
Gabriela Tiago de Araújo <gabrielatiagodearaujo@outlook.com>
2024-05-31 01:12:59 -0300

    ✨ feat: Cria módulo dos temporizadores

---
+++ src/modules/temporizadores.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+def pausa_curta():
+    sleep(0.020)
+
+def aguarda_carregamento_rapido():
+    sleep(3)
+
+def aguarda_carregamento_medio():
+    sleep(7)
+
+def aguarda_carregamento_lento():
+    sleep(20)
